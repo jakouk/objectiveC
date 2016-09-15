8dc061a9a9781530fa4981d8753e8ed78a2b382f
jakouk <jakoriaty@gmail.com>
2016-09-15 20:31:15 +0900

    Fisrt Commit add FirstProject

---
+++ FirstProject/FirstProject/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  FirstProject
+//
+//  Created by jakouk on 2016. 9. 15..
+//  Copyright © 2016년 jakouk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"안녕하t십니까. 키미니 입니다 ㅋㅋㅋ");
+        NSLog(@"Hello Xcode");
+    }
+    return 0;
+}
