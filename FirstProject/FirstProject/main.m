85a3bdaabcb7e0d7851050ced3ee6b9af5cbf203
jakouk <jakoriaty@gmail.com>
2016-09-16 21:09:22 +0900

    update FirstProject/ main.c

--- FirstProject/FirstProject/main.m
+++ FirstProject/FirstProject/main.m
@@ -11,12 +11,14 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        
         /*
         NSLog(@"Hello, World!");
         NSLog(@"안녕하십니까. 키미니 입니다 ㅋㅋㅋ");
         NSLog(@"Hello Xcode");
          */
         
+        /*
         int val_integer = 0;
         float val_float = 0.0f;
         double val_double = 0.0;
@@ -31,6 +33,8 @@ int main(int argc, const char * argv[]) {
         NSLog(@"The first time float        %f ",val_float);
         NSLog(@"The first time double       %lf ",val_double);
         NSLog(@"The first time char         %c ",val_char);
+         
+        //정수는 %d, 실수중 float 은 %f, double은 %lf, 문자 char 는 %c, 문자열은 %s
         
         NSLog(@"Integer can be converted to character       %c ",val_integer);
         NSLog(@"Character can be converted to integer       %d \n",val_char);
@@ -70,7 +74,23 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"Array of char char_array[0] : %c, char_array[1] : %c, char_array[2] : %c, char_array[3] : %c, char_array[4] : %c\n", char_array[0], char_array[1], char_array[2], char_array[3], char_array[4] );
         
-        NSLog(@"Character array can be used as String : %s", char_array);
+        NSLog(@"Character array can be used as String : %s", char_array);   
+         // 문자열은 char배열이고 마지막은 \0 으로 끝난다.
+        
+        */
+        
+        double val_double = 3532.048394;
+        
+        //%%는 %를 표현하기 위한것
+        NSLog(@"%%lf    : %lf",val_double);
+        NSLog(@"%%.3lf  : %.3lf",val_double);       //소수점 3자리
+        NSLog(@"%%.9lf  : %.9lf", val_double);      //소수점 9자리
+        NSLog(@"%%10.3lf: %10.3lf", val_double);    //10칸 표현하고 3칸은 소수 -> 정수 6자리, 소수 3자리
+        NSLog(@"%%3.3lf : %5.3lf", val_double);     //3칸 표현 -> 만약 표현되는 숫자가 더 크면 무시하고 전부 표현
+        NSLog(@"%%10lf  : %10lf", val_double);      //10칸 표현 -> 10칸 넘어가면 전부 표현됨
+        NSLog(@"%%15lf  : %15lf", val_double);      //15칸 표현 -> 15칸 표현되는데 4칸 남아서 빈칸으로 표현
+        NSLog(@"%%-15lf : %-15lf", val_double);     //15칸 표현, 좌측정렬
+        NSLog(@"%%+15lf : %+15lf",val_double);      //아무 의미 없음.
         
         
     }
